d5715f630b7f695c5dc813cad6bc83d9273afbc1
Uday Kumar <kuday0726@gmail.com>
2025-11-21 11:17:23 +0000

    feat(escrow): add deposit,populate escrow functions

--- blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/instructions/make.rs
+++ blueshift_anchor_escrow/programs/blueshift_anchor_escrow/src/instructions/make.rs
@@ -1,2 +1,93 @@
 #[allow(unexpected_cfgs)]
 #[allow(deprecated)]
+use anchor_lang::prelude::*;
+use crate::{errors::EscrowError, state::Escrow};
+use anchor_spl::{Mint, associated_token::AssociatedToken, token::{TokenAccount, spl_token::instruction::transfer_checked, TransferChecked}};
+
+#[derive(Accounts)]
+#[instruction(seed: u64)]
+pub struct Make<'info> {
+    #[account(mut)]
+    pub maker: Signer<'info>,
+    #[account(
+        init,
+        payer = maker,
+        space = Escrow::INIT_SPACE + Escrow::DISCRIMINATOR.len(),
+        seeds = [b"escrow",maker.key().as_ref(), seeds.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub escrow: Account<'info,Escrow>,
+    //Token accounts
+    #[account(
+        mint::token_program = token_program
+    )]
+    pub mint_a: InterfaceAccount<'info,Mint>,
+    #[account(
+        mint::token_program = token_program
+    )]
+    pub mint_b: InterfaceAccount<'info,Mint>,
+    #[account(
+        mut,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub maker_ata_a: InterfaceAccount<'info,TokenAccount>,
+    #[account(
+        init,
+        payer = maker,
+        associated_token::mint = mint_a,
+        associated_token::authority = escrow,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info,TokenAccount>,
+    pub associated_token_program: Program<'info,AssociatedToken>,
+    pub token_program: Program<'info,TokenAccount>,
+    pub system_prorgam: Program<'info,System>,
+}
+
+impl<'info> Make<'info>{
+    //initialze escrow
+    fn populate_escrow(&mut self, seed:u64, amount: u64, bump: u8)->Result<()>{
+        self.escrow.set_inner(Escrow { 
+            seed, 
+            maker: self.maker.key(), 
+            mint_a: self.mint_a.key(), 
+            mint_b: self.mint_b.key(), 
+            recieve: amount,
+            bump
+        });
+        Ok(())
+    }
+    /// # Deposit the tokens
+    fn deposit_tokens(&self, amount: u64)-> Result<()>{
+        transfer_checked(
+            CpiContext::new(
+                self.token_program.to_account_info(),
+                TransferChecked{
+                    from: self.maker_ata_a.to_account_info(),
+                    mint: self.mint_a.to_account_info(),
+                    to: self.vault.to_account_info(),
+                    authority: self.maker.to_account_info()
+                },
+            ),
+            amount,
+            self.mint_a.decimals,
+        )?;
+        Ok(())
+    }
+    // Depost token
+    //why using transfer checked?
+
+}
+
+pub fn handler(ctx: Context<Make>, seed: u64, recieve:u64, amount: u64)->Result<()>{
+    //validate the amount
+    require_gt!(recieve,0,EscrowError::InvalidAmount);
+    require_gt!(amount,0, EscrowError::InvalidAmount);
+    //save the escrow data
+    ctx.accounts.populate_escrow(seed, recieve, ctx.bumps.escrow);
+    //deposit Tokens
+    ctx.accounts.deposit_tokens(amount)?;3
+    Ok(())
+}
